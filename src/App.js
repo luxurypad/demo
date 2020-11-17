80bc0ae6d05504fc9aab46d78f24149a010f932e
luxurypad <luxurypad@icloud.com>
2020-11-17 14:16:29 +0800

    1.将server文件夹移到根目录，作为单独的json-server；2.添加react-markdown包，新建article组件

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import CssBaseline from './components/common/CssBaseline'
 import Student from './components/student'
+import Article from './components/article'
 import Test from './components/test'
 
 export default () => {
@@ -12,10 +13,14 @@ export default () => {
         <nav>
           <Link to='/xuexi'>学习</Link>
           <Link to='/test'>测试</Link>
+          <Link to='/article'>文章</Link>
         </nav>
         <Switch>
           <Route exact path='/xuexi'>
             <Student />
+          </Route>
+          <Route exact path='/article'>
+            <Article />
           </Route>
           <Route exact path='/test'>
             <Test />
---
+++ src/components/article/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+  root: {
+    '& pre': {
+      backgroundColor: '#ecf0f1',
+      padding: '1rem',
+      borderRadius: '.5rem'
+    },
+    '& code': {
+      backgroundColor: '#ecf0f1',
+    },
+    '& blockquote': {
+      backgroundColor: 'rgba(0,0,0,0.06)',
+      marginLeft: '1rem',
+      marginBottom: '.3rem',
+      borderLeft: '.3rem solid #3498db'
+    }
+  }
+})
+
+export default () => {
+  const [data, setData] = useState('')
+  const classes = useStyles()
+  useEffect(() => {
+    fetch('http://localhost:5000/笔记.md', {
+      method: 'GET'
+    })
+      .then(response => {
+        return response.text()
+      })
+      .then(data => {
+        setData(data)
+      })
+  }, [data])
+
+  return (
+    <div className={classes.root}>
+      <ReactMarkdown plugins={[gfm]}>
+        {data}
+      </ReactMarkdown>
+    </div>
+  )
+}
